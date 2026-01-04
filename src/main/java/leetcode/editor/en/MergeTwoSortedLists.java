c2d3bbc83fa334994d64504d6945928b74640d62
TwilightVoyager777 <TwilightVoyager777@gmail.com>
2026-01-03 23:07:34 -0800

    Initialize the repository directory

---
+++ src/main/java/leetcode/editor/en/MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+package leetcode.editor.en;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+public class MergeTwoSortedLists {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        // put your test code here
+        
+    }
+}
